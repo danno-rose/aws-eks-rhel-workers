8478ffec61f9c6d1ee2ac7dbd7aaa554477d0817
Rose <drosea@amazon.co.uk>
2019-10-23 13:39:25 +0100

    remove unwanted script

--- install-centos-requirements.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo yum install -y python-pip
-sudo pip install --upgrade awscli
-sudo yum install -y container-selinux
-sudo yum install updates
